6d6000d9465931fa62dc8b86342eeedada2be7f2
Marek Jędrzejewski <mjedrzejewski92@gmail.com>
2017-12-11 19:11:12 +0100

    Add multiple currencies support

--- zmalaubot.py
+++ zmalaubot.py
@@ -1,9 +1,15 @@
-#!/bin/python3
+#!/usr/bin/python3
 
+# ₿Ξ
 from urllib import request
 import json
 from matrix_client.client import MatrixClient
 import sys
+
+SUPPORTED_FIAT = ['USD', 'EUR', 'PLN']
+DEFAULT_FIAT = 'USD'
+SUPPORTED_CRYPTO = ['BTC', 'ETH']
+DEFAULT_CRYPTO = 'BTC'
 
 if len(sys.argv) != 4:
     print("USAGE: ./zmalaubot.py username password room")
@@ -13,47 +19,75 @@ username = sys.argv[1]
 password = sys.argv[2]
 roomname = sys.argv[3]
 
-def check_current_price():
-    return json.load(request.urlopen("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR"))
 
-last_price = check_current_price()
+class ZmalauBot():
+    def __init__(self, username, password, roomname):
+        # init all cryptos on start
+        self.last_price = {}
+        for crypto in SUPPORTED_CRYPTO:
+            self.last_price[crypto] = self.check_current_price(crypto)
 
-client = MatrixClient("http://matrix.org")
+        # connect to room
+        self.client = MatrixClient("http://matrix.org")
+        self.token = self.client.login_with_password(username=username,
+                                                     password=password)
+        self.room = self.client.join_room(roomname)
 
-token = client.login_with_password(username=username, password=password)
+        # add bot reactions
+        self.room.add_listener(self.on_message)
+        self.client.start_listener_thread()
 
-room = client.join_room(roomname)
+    @staticmethod
+    def check_current_price(crypto_symbol):
+        return json.load(request.urlopen("https://min-api.cryptocompare.com/"
+                                         f"data/price?fsym={crypto_symbol}"
+                                         f"&tsyms={','.join(SUPPORTED_FIAT)}"))
+
+    def on_message(self, room, event):
+        if event['type'] == "m.room.message":
+            if username in event['sender']:
+                return
+            if event['content']['msgtype'] == "m.text":
+                message = event['content']['body']
+                m = message.lower()
+                if 'zmalau' in m or 'urus' in m:
+                    self.analyze_message_and_respond(message)
+
+    def analyze_message_and_respond(self, message):
+        m = message.upper()
+        used_crypto = [c for c in SUPPORTED_CRYPTO if c in m]
+        used_fiat = [f for f in SUPPORTED_FIAT if f in m]
+
+        if not used_crypto:
+            used_crypto = [DEFAULT_CRYPTO]
+        if not used_fiat:
+            used_fiat = [DEFAULT_FIAT]
+
+        fiat = used_fiat[0]
+
+        for crypto in used_crypto:
+            current_price = self.check_current_price(crypto)
+            change = current_price[fiat] / self.last_price[crypto][fiat] - 1
+            if change > 0:
+                status = f'{crypto} urus.'
+            elif change < 0:
+                status = f'{crypto} zmalau.'
+            else:
+                status = f'{crypto} stoi.'
+
+            change *= 100
+            if change != 0:
+                status += f' zmiana: {change:.4f}%'
+            for fiat in used_fiat:
+                status += '\n1 {crypto} = {amount} {currency} '.format(crypto=crypto,
+                                                                      amount=current_price[fiat],
+                                                                      currency=fiat)
+                self.last_price[crypto] = current_price
+            self.room.send_text(status)
 
 
-def on_message(room, event):
-    global last_price
-    if event['type'] == "m.room.message":
-        if username in event['sender']:
-            return
-        if event['content']['msgtype'] == "m.text":
-            message = event['content']['body']
-            if 'zmalau' in message or 'urus' in message:
-                current_price = check_current_price()
-                change = current_price['USD'] / last_price['USD'] - 1
-                if change > 0:
-                    status = 'urus'
-                elif current_price['USD'] < last_price['USD']:
-                    status = 'zmalau'
-                else:
-                    status = 'stoi'
-                room.send_text('{status}. zmiana: {change:.4f}%, cena: {usd}USD'
-                        .format(status=status,
-                                change=change*100,
-                                usd=current_price['USD']))
-                last_price = current_price
-
-
-room.add_listener(on_message)
-client.start_listener_thread()
-
+zmalaubot = ZmalauBot(username, password, roomname)
 while True:
     msg = input()
     if msg == "/quit":
         break
-    else:
-        room.send_text(msg)
